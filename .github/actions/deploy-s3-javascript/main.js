6190b67555b23204e0c61f99f20e91dfe855323b
tejaswitaw <31151862+TejaswitaW@users.noreply.github.com>
2024-08-02 05:14:49 +0530

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,20 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // Get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
     core.notice('Hello from my custom Javascript Action');
+    core.notice('Uploading static site to AWS s3');
+
+    // Using these inputs we upload files
+    // creating identifier of S3 bucket, that path is constructed dynamically
+    const s3Uri = `s3://${bucket}`
+    // exec.exec('aws s3 sync <localFolder> <s3-bucket path> ')
+    // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+    exec.exec(`echo "Dist-Folder: $distFolder S3Uri: $s3Uri Bucket-Region: $bucketRegion"`)
+  
 }
 
 run();
